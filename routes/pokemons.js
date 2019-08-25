305469de95964ef4ef7355b93910c931384623da
Nomi <naumanahmed19@gmail.com>
2019-08-25 11:31:54 +0200

    importer added

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -1,7 +1,10 @@
+const config = require('config');
 const express = require('express');
 const { Pokemon } = require('../models/pokemon');
+const axios = require('axios');
 
 const router = express.Router();
+
 
 router.get('/', async (req, res) => {
     const pokemons = await Pokemon.find().sort('name');
@@ -13,19 +16,39 @@ router.get('/:id', async (req, res) => {
     res.send(pokemons)
 })
 
-router.post('/', async (req, res) => {
-    try {
-        let pokemon = new Pokemon({
-            image: req.body.image,
-            name: req.body.name,
-            weight: req.body.weight,
-            height: req.body.height,
-        });
-        res.send(pokemon)
 
-    } catch (error) {
-        console.log(error);
-    }
-});
+
+async function importPokemon(url) {
+    await axios.get(url)
+        .then(function (res) {
+            try {
+                let pokemon = new Pokemon({
+                    image: res.data.sprites.front_default,
+                    name: res.data.name,
+                    weight: res.data.weight,
+                    height: res.data.height,
+                });
+
+                const result = pokemon.save()
+
+            } catch (error) {
+                console.log(error.message);
+            }
+        });
+}
+
+
+
+router.get('/import/data', async (req, res) => {
+    await axios.get(config.get('pokeApi'))
+        .then(function (response) {
+            response.data.results.forEach(async (r) => {
+                await importPokemon(r.url);
+            });
+            res.send({ 'message': 'Data imported successfully' })
+        });
+})
+
+
 
 module.exports = router;
